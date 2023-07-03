26054ba748ced8f981102b15ead4f70ffe8d26b4
Aaron Nevalinz <aaronug421@gmail.com>
2023-07-03 12:24:11 +0300

    Learnt basics of tailwindcss through this.

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#ff6363',
+        secondary:{
+          100:'#E2E2d5',
+          200:'#888883'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
